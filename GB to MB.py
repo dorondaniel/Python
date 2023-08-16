7531d53373d020694e2ac75ff8e666a192b32169
doronpc <dorondaniel.n.c.2020.cse@ritchennai.edu.in>
2023-08-16 18:52:31 +0530

    new file:   GB to MB.py new file:   sliding_window.py

---
+++ GB to MB.py
@@ -0,0 +1,9 @@
+x = int(input("Enter the GIGABYTES value: "))
+i = 0
+
+if x>0:
+    mb = x * 1028
+    print("The Values in MEGABYTES: ",mb)
+
+print(input("Press Enter"))
+    
---
+++ sliding_window.py
@@ -0,0 +1,23 @@
+import sys
+k = int(input("Enter Window Size: "))
+
+lst= [2,14,1,34,22,12,5,3]
+
+wSum = 0
+mxSum = -sys.maxsize - 1 # MinNegativeInteger
+if(k>len(lst)):
+    print()
+
+for i in range(k):
+    wSum += lst[i]
+mxSum = max(wSum,mxSum)
+
+#Static Window
+
+for j in range(k,len(lst)-1,1):
+    wSum = wSum - lst[j-k] + lst[j]
+    mxSum = max(wSum,mxSum)
+
+#Window End
+
+print("Max Sum of a Subarray of the size {} is : {} ".format(k,mxSum))
